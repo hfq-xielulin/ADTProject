769938ad2b3229bfdd3d4431fa43772418f290a4
hfq-xielulin <xielulin@huizhaofang.com>
2017-12-14 18:25:44 +0800

    二叉查找树的简单实现

--- ADTProkject/src/com/xll/adt/main/StackTest.java
+++ ADTProkject/src/com/xll/adt/main/StackTest.java
@@ -2,7 +2,11 @@ package com.xll.adt.main;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
+import com.xll.adt.adt.BinarySerachTree;
 import com.xll.adt.adt.LinkList;
 import com.xll.adt.adt.LinkedList;
 import com.xll.adt.adt.ListQueue;
@@ -15,9 +19,32 @@ import com.xll.adt.adt.SortedList;
 @SuppressWarnings("unused")
 public class StackTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
+		doBinarySearchTree();
+	}
+	private static void doBinarySearchTree() throws Exception {
+		BinarySerachTree<Integer> tree=new BinarySerachTree<Integer>();
+		tree.insert(25);
+		tree.insert(10);
+		tree.insert(15);
+		tree.insert(30);
+		tree.insert(35);
+		tree.insert(23);
+	
+		tree.printTree();
+		System.out.println("aaaaaaaaaaa");
+		System.out.println(tree.finMax());
+	
+		
+		System.out.println(	tree.finMin());
+		System.out.println("aaaaaaaaaaa");
+		tree.remove(30);
+		
+		tree.printTree();
+	}
+	public void doADT() {
 
-		doStack();
+		/*doStack();
 		doqueue();
 		doPriQueue();
 		doList();
@@ -26,7 +53,66 @@ public class StackTest {
 		doListQueue();
 		doSortedList();
 
-		doLinkedList();
+		doLinkedList();*/
+		
+		
+	/*	String key ="1234aA";
+		
+		if(!verifyString(key, "(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])[a-zA-Z0-9]{8,32}")) {
+			System.out.println("不符合标准");
+		}else {
+			System.out.println("符合标准");
+		}
+		if(!verifyString(key, ".*\\d+.*")) {
+			System.out.println("密码必须包含数字:" + key);
+		}
+		if(verifyString(key, "^[A-Z]+$")) {
+			System.out.println("密码必须包含大写字母:" + key);
+		}
+		if(verifyString(key, "^[a-z]+$")) {
+			System.out.println("密码必须包含小写字母:" + key);
+		}else
+			System.out.println("验证完毕");
+		
+		System.out.println(getRandomString(19));*/
+	}
+	
+	
+	public static String getRandomString(int length) {
+		String sb;
+		String str1="abcdefghijklmnopqrstuvwxyz";
+		String str2="1234567890";
+		String str3="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		String str4="1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		sb=getString(1,str1)+getString(1,str3)+getString(1,str2)+getString(length-3,str4);
+		return sb;
+	}
+
+	/**
+	 * 
+	 * @author:xielulin 
+	 * @Description: 生成随机字符串
+	 * @param length
+	 * @param s
+	 * @return:
+	 * @throws:
+	 * @date:2017年12月7日 上午9:52:08
+	 */
+	public static String getString(int length,String s) {
+		StringBuffer buffer = new StringBuffer(s);
+		StringBuffer sb = new StringBuffer();
+		Random r = new Random();
+		int range = buffer.length();
+		for (int i = 0; i < length; i++) {
+			sb.append(buffer.charAt(r.nextInt(range)));
+		}
+		return sb.toString();
+	}
+
+	private static boolean verifyString(String cs, String regex) {
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(cs);
+		return matcher.matches();
 	}
 
 	private static void doLinkedList() {
