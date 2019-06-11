5a355a9779042fec4b4e9bbf8e581145a590fba5
xielulin <656600528@qq.com>
2019-06-11 16:12:43 +0800

    新增一个求最小深度的方法

--- ADTProject/src/com/xll/adt/demo/Demo.java
+++ ADTProject/src/com/xll/adt/demo/Demo.java
@@ -24,6 +24,13 @@ public class Demo {
         }
     }
 
+    /**
+     * @description 计算最小深度
+     * @author xielulin
+     * @date 2019/6/11
+     * @param root
+     * @return int
+     */
     public static int MinDepth(Node root){
         if(root == null){
             return 0;
@@ -38,4 +45,18 @@ public class Demo {
         int rightDepth = MinDepth(root.right);
         return 1 + Math.min(leftDepth,rightDepth);
     }
+
+    public static int MinDepth2(Node root){
+        if(root == null){
+            return 0;
+        }
+        int leftDeep = MinDepth2(root.left);
+        int rightDeep = MinDepth2(root.right);
+        if(leftDeep == 0 || rightDeep == 0){
+            return leftDeep+rightDeep+1;
+        }else{
+            return Math.min(leftDeep,rightDeep)+1;
+        }
+    }
+
 }
